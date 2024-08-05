909f1ebea44b31b10c946023e6d1727125a6ed19
Pinto <princetonpinto94@gmail.com>
2024-08-05 14:01:58 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -109,6 +109,29 @@ console.log(date.toLocaleTimeString());
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    // console.log(response);
+    return response.json();
+  }).then((productsData) => {
+    // console.log(productsData);
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    //console.log(products);
+    console.log('load products');
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
